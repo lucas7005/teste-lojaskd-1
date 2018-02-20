818481b9c090ec4d16913115b93d47b6ca7e1b05
Lucas Borim <lucas_7005@hotmail.com>
2018-02-20 12:04:46 -0300

    Upload Teste 1

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+
+//Direciona e sinaliza o banner para qual categoria está selecionada
+
+function funcao(id){
+
+    switch (id) {
+        case 1:
+            document.getElementById("6m2").src = "./assets/images/6m2-Selected.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            console.log("test1");
+            break;
+
+        case 2:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2-Selected.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            break;
+        case 3:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2-Selected.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            break;
+
+        case 4:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2-Selected.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            break;
+        
+        case 5:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2-Selected.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            break;
+
+        case 6:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2-Selected.png";
+            document.getElementById("12m2").src = "./assets/images/12m2.png";
+            break;
+
+        case 7:
+            document.getElementById("6m2").src = "./assets/images/6m2.png";
+            document.getElementById("7m2").src = "./assets/images/7m2.png";
+            document.getElementById("8m2").src = "./assets/images/8m2.png";
+            document.getElementById("9m2").src = "./assets/images/9m2.png";
+            document.getElementById("10m2").src = "./assets/images/10m2.png";
+            document.getElementById("11m2").src = "./assets/images/11m2.png";
+            document.getElementById("12m2").src = "./assets/images/12m2-Selected.png";
+            break;
+    }
+    
+}
+
+
